ce8fcea38ddb593e072509e4fe5c6dd145e42925
Busra <soylemezbusrauk@gmail.com>
2022-08-16 12:20:49 +0100

    day1 basic navigations

--- src/test/java/com/cydeo/tests/day_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day_selenium_intro/BasicNavigations.java
@@ -1,8 +1,45 @@
 package com.cydeo.tests.day_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigations {
 
+    public static void main(String[] args) throws InterruptedException {
 
+        //1- Set up the browser driver
+        WebDriverManager.chromedriver().setup();
+
+        //2- Create instance of  the Selenium webDriver
+        WebDriver driver = new ChromeDriver();
+
+        //3-- Go to "https://www.tesla.com"
+        driver.get("https://www.tesla.com");
+
+        //Stop code execution  for 3 seconds
+        Thread.sleep(3000);
+
+
+        // use selenium to navigate back
+        driver.navigate().back();
+
+        //Stop code execution  for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate forward
+        driver.navigate().forward();
+
+        //Stop code execution  for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+
+
+
+    }
 
 
 }
