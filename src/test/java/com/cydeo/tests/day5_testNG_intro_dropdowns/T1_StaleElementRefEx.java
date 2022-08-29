94b53d06d8730c026c79e5f213c0ad1e612584b3
Busra <soylemezbusrauk@gmail.com>
2022-08-29 15:00:04 +0100

    day5 Task1

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -1,13 +1,16 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class T1_StaleElementRefEx {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #1: StaleElementReferenceException handling
         //1. Open Chrome browser
@@ -21,12 +24,36 @@ public class T1_StaleElementRefEx {
 
 
         //3. Click to “Add Element” button
+        WebElement addElementBtn= driver.findElement(By.xpath("//button[.='Add Element']"));
+
+        Thread.sleep(2000);
+        addElementBtn.click();
 
 
         //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton= driver.findElement(By.xpath("//button[@class='added-manually']"));
+
+
         //5. Click to “Delete” button.
+        deleteButton.click();
+
+
+
         //6. Verify “Delete” button is NOT displayed after clicking.
-        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+
+       try{
+           System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+       }catch (StaleElementReferenceException e){
+
+           System.out.println("--> StaleElementReferenceException exception is thrown");
+           System.out.println("--> This means the web element is completely deleted from the page");
+           System.out.println("-->deleteButton.isDisplayed() = false ");
+
+        }
+
+
+
+        //USE XPATH LOCATOR FOR ALL WEB ELEMENT LOCATORS
 
 
 
