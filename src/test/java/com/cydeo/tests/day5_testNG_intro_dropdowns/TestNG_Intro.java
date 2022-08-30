34ad130808f1aa2581de2b27e0fbcead43e05e90
Busra <soylemezbusrauk@gmail.com>
2022-08-30 16:08:00 +0100

    day5 test NG

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,8 +1,29 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @Test (priority = 1)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+
+
+        //ASSERT EQUALS : compare 2 of the same things
+       String actual = "apple";
+       String expected = "apple";
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+    }
+
 
     @BeforeClass
     public void setUpMethods(){
@@ -27,15 +48,6 @@ public class TestNG_Intro {
     }
 
 
-    @Test (priority = 1)
-    public void test1(){
-        System.out.println("Test 1 is running...");
-    }
-
-    @Test (priority = 2)
-    public void test2(){
-        System.out.println("Test 2 is running...");
-    }
 
 
 }
