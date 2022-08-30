17786aee4083fec8fd4e9d2e588fbe93d2af02ee
Busra <soylemezbusrauk@gmail.com>
2022-08-30 16:00:34 +0100

    day5 test NG

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,15 +1,38 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @Test
+    @BeforeClass
+    public void setUpMethods(){
+        System.out.println("---> BeforeClass is running");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("---> AfterClass is running");
+
+    }
+
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> Before Method is running...");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--> AfterMethod is running!");
+    }
+
+
+    @Test (priority = 1)
     public void test1(){
         System.out.println("Test 1 is running...");
     }
 
-    @Test
+    @Test (priority = 2)
     public void test2(){
         System.out.println("Test 2 is running...");
     }
