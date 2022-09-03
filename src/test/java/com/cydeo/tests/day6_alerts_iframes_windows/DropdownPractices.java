e581053c9aec37fa95d8c2d692a083dbbba62826
Busra <soylemezbusrauk@gmail.com>
2022-09-03 13:33:59 +0100

    day6 test6 state dropdown

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,7 +14,7 @@ public class DropdownPractices {
     WebDriver driver;
 
     @Test
-    public void dropdown_task5(){
+    public void dropdown_task5() throws InterruptedException {
         //1. Open Chrome browser
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
@@ -21,10 +24,27 @@ public class DropdownPractices {
         //2. Go to http://practice.cybertekschool.com/dropdown
         driver.get("http://practice.cybertekschool.com/dropdown");
 
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
         //3. Select Illinois
+        Thread.sleep(1000);
+        stateDropdown.selectByVisibleText("Illinois");
+
         //4. Select Virginia
+        Thread.sleep(1000);
+        stateDropdown.selectByValue("VA");
+
         //5. Select California
+        Thread.sleep(1000);
+         stateDropdown.selectByIndex(5);
+
         //6. Verify final selected option is California.
+        String expectedOptionText= "California";
+
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualOptionText,expectedOptionText);
+
         //Use all Select options. (visible text, value, index)
 
 
