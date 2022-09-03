40ca7d3412f70c797cb855351a67eb0ac05b2b05
Busra <soylemezbusrauk@gmail.com>
2022-09-03 13:03:50 +0100

    day6 test6 state dropdown

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    WebDriver driver;
+
+    @Test
+    public void dropdown_task5(){
+        //1. Open Chrome browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        //2. Go to http://practice.cybertekschool.com/dropdown
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+        //3. Select Illinois
+        //4. Select Virginia
+        //5. Select California
+        //6. Verify final selected option is California.
+        //Use all Select options. (visible text, value, index)
+
+
+
+    }
+
+
+}
